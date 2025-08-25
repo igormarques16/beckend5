8d1861fedcd751d983cb4a175b8b461db475e9bd
igormarques16 <i.marques@iesb.edu.br>
2025-08-25 20:00:29 -0300

    calculadora

---
+++ CONSTRUCAO_BACKEND-main/Exercicios/03-Calculadora/calculadora.js
@@ -0,0 +1,40 @@
+// calculadora.js
+
+function somar(a, b) {
+    return a + b;
+}
+
+function subtrair(a, b) {
+    return a - b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    if (b === 0) {
+        return 'Erro: divisão por zero';
+    }
+    return a / b;
+}
+
+function aoQuadrado(a) {
+    return a * a;
+}
+
+function raizQuadrada(a) {
+    if (a < 0) {
+        return 'Erro: raiz quadrada de número negativo';
+    }
+    return Math.sqrt(a);
+}
+
+module.exports = {
+    somar,
+    subtrair,
+    multiplicar,
+    dividir,
+    aoQuadrado,
+    raizQuadrada
+};
---
+++ CONSTRUCAO_BACKEND-main/Praticas/01-primeiro-projeto/index.js
@@ -0,0 +1,16 @@
+console.log ("meu primeiro projeto")
+
+let nome = 'Lucas'
+
+let sobreNome = "Lopes"
+
+let idade = "25"
+
+console.log ("Olá " + nome + " " + sobreNome)
+console.log ("Sua idade é " + idade)
+
+if(idade >= 18){
+    console.log ("Você é maior de idade!!!")
+} else {
+    console.log ("Você é menor de idade!!!")
+}
